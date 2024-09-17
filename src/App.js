d8e74b6f5b1ec050c0d7dc1540256f78533f7dfc
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-09-17 20:57:38 +0100

    Updated footer code

--- src/App.js
+++ src/App.js
@@ -7,28 +7,32 @@ function App() {
         <h1>My Weather App</h1>
         <Weather city="Paris" />
       </header>
-      <footer>
-        <p>
-          Coded by
-          <a href="https://github.com/MegNia2" target="_blank">
-            Megnia
-          </a>
-          on
-          <a
-            href="https://github.com/MegNia2/megnia-weather-project-react"
-            target="_blank"
-          >
-            Github
-          </a>
-          , hosted on
-          <a
-            href="https://megnia-weather-project-react.netlify.app"
-            target="_blank"
-          >
-            Netlify
-          </a>
-        </p>
-      </footer>
+      <p>
+        Coded by
+        <a
+          href="https://github.com/MegNia2"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Megnia
+        </a>
+        on
+        <a
+          href="https://github.com/MegNia2/megnia-weather-project-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        , hosted on
+        <a
+          href="https://megnia-weather-project-react.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
+      </p>
     </div>
   );
 }
