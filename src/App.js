99200bb832049d006558399adf4ba48f24b875de
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-09-17 20:51:37 +0100

    Added links to github and netlify

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ function App() {
         <h1>My Weather App</h1>
         <Weather city="Paris" />
       </header>
+      <footer>
+        <p>
+          Coded by{" "}
+          <a href="https://github.com/MegNia2" target="_blank">
+            Megnia
+          </a>{" "}
+          on
+          <a
+            href="https://github.com/MegNia2/megnia-weather-project-react"
+            target="_blank"
+          >
+            Github
+          </a>
+          , hosted on{" "}
+          <a
+            href="https://megnia-weather-project-react.netlify.app"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
